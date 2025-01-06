be686eb9f5ccc0d69ed83292f4deffae80c44c79
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-01-06 15:35:03 +0530

    Enhance CartContext to persist cart items in localStorage and update on changes

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,10 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem("Odin Cart Items");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      localStorage.setItem("Odin Cart Items", JSON.stringify(cart));
+    } else {
+      localStorage.removeItem("Odin Cart Items");
+    }
+  }, [cart]);
 
   return (
     <CartContext.Provider value={[cart, setCart]}>
