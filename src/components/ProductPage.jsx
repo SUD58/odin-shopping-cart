a2a238e10d371a5eb3cc457170002828905c7ff9
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-01-07 16:35:06 +0530

    Enhance ProductPage component; improve layout, add reviews section, and update styling for better readability

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -59,7 +59,7 @@ export default function ProductPage() {
           {loading && <p>Loading...</p>}
           {error && <p>Error: {error.message}</p>}
 
-          <div className="grid w-full grid-cols-12 p-6">
+          <section className="grid w-full grid-cols-12 p-6">
             <div className="relative col-span-3">
               {product.images && product.images.length > 0 ? (
                 <div
@@ -76,34 +76,70 @@ export default function ProductPage() {
                       />
                     </div>
                   ))}
-                  <button
-                    onClick={scrollToPrevious}
-                    className="absolute left-0 top-1/2"
-                  >
-                    <i className="fas fa-chevron-circle-left fa-2xl"></i>
-                  </button>
-                  <button
-                    onClick={scrollToNext}
-                    className="absolute right-0 top-1/2"
-                  >
-                    <i className="fas fa-chevron-circle-right fa-2xl"></i>
-                  </button>
+                  {product.images.length > 1 ? (
+                    <>
+                      <button
+                        onClick={scrollToPrevious}
+                        className="absolute left-0 top-1/2"
+                      >
+                        <i className="fas fa-chevron-circle-left fa-2xl"></i>
+                      </button>
+                      <button
+                        onClick={scrollToNext}
+                        className="absolute right-0 top-1/2"
+                      >
+                        <i className="fas fa-chevron-circle-right fa-2xl"></i>
+                      </button>
+                    </>
+                  ) : null}
                 </div>
               ) : (
                 <p>No images available</p>
               )}
             </div>
-
-            <div className="col-span-7">
-              <h1>{product.title}</h1>
-              <h2>{product.brand}</h2>
-              <p>{product.description}</p>
+            <div className="col-span-7 space-y-4">
+              <div className="space-y-1">
+                <h1 className="text-2xl font-bold">{product.title}</h1>
+                <h2 className="text-sm font-bold text-zinc-600">
+                  {product.brand}
+                </h2>
+              </div>
+              <p className="max-w-[60ch] text-pretty">{product.description}</p>
             </div>
-
-            <div className="col-span-2 rounded-2xl bg-zinc-100">
+            <div className="col-span-2 flex flex-col justify-between self-start rounded-2xl bg-zinc-100 p-6">
+              <h3>${product.price}</h3>
+              {product.stock > 0 ? <p>In stock!</p> : <p>Out of stock</p>}
               <AddToCartButton product={product} />
             </div>
-          </div>
+          </section>
+
+          <section className="flex flex-col gap-4 p-6">
+            <h2 className="text-xl font-bold">Reviews</h2>
+            {product.reviews && product.reviews.length > 0 ? (
+              product.reviews.map((review, index) => (
+                <div key={index} className="flex flex-col gap-2 border-b-2">
+                  <div className="flex gap-2">
+                    <span>{review.rating}/5</span>
+                    <div>
+                      {Array.from({ length: review.rating }).map((_, i) => (
+                        <i key={i} className="fa fa-star text-yellow-400"></i>
+                      ))}
+                      {Array.from({ length: 5 - review.rating }).map((_, i) => (
+                        <i key={i} className="fa fa-star"></i>
+                      ))}
+                    </div>
+                  </div>
+
+                  <p className="text-lg font-bold">{review.comment}</p>
+                  <p className="text-sm font-bold text-zinc-600">
+                    {review.reviewerName}
+                  </p>
+                </div>
+              ))
+            ) : (
+              <p>No reviews available</p>
+            )}
+          </section>
         </CartProvider>
       </CartVisibilityProvider>
     </>
